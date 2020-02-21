bc939208a0ebbe42f2bff863b86b4dee67766f7f
Lily Scott <me@suchipi.com>
2020-02-21 01:03:10 -0700

    Use external packages for nwjs binaries

--- server/bin.js
+++ server/bin.js
@@ -1,40 +1,18 @@
 "use strict";
 
 const debug = require("debug")("node-nw:server/bin");
-const path = require("path");
 const nodeNw = require("./index");
 
 let bin;
 if (process.platform === "win32") {
   debug("Platform detected: win32");
-  bin = path.resolve(
-    __dirname,
-    "..",
-    "binaries",
-    "nwjs-sdk-v0.44.2-win-x64",
-    "nw.exe"
-  );
+  bin = require("@nwjs-binaries/win-x64");
 } else if (process.platform === "darwin") {
   debug("Platform detected: darwin");
-  bin = path.resolve(
-    __dirname,
-    "..",
-    "binaries",
-    "nwjs-sdk-v0.44.2-osx-x64",
-    "nwjs.app",
-    "Contents",
-    "MacOS",
-    "nwjs"
-  );
+  bin = require("@nwjs-binaries/osx-x64");
 } else if (process.platform === "linux") {
   debug("Platform detected: linux");
-  bin = path.resolve(
-    __dirname,
-    "..",
-    "binaries",
-    "nwjs-sdk-v0.44.2-linux-x64",
-    "nw"
-  );
+  bin = require("@nwjs-binaries/linux-x64");
 } else {
   console.error("NW.js is not supported on this platform:", process.platform);
   process.exit(1);
